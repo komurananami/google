b3b27c8bac0fc266ce3b291a0f18377f2e0a85ba
nanami <kom_nana5102@icloud.com>
2021-04-07 15:34:50 +0900

    add header elements

--- plain/object/js.js
+++ plain/object/js.js
@@ -18,8 +18,35 @@ const App = {
       const els = App.elements;
       console.log(" Lets create the base elements");
 
-      els.header.innerHTML = "header";
+      els.header.style.display = "flex";
+      els.header.style.justifyContent = "space-between";
+      els.header.style.padding = "0 80px";
+      els.header.style.height = "100px";
+      els.header.style.zIndex = 10;
+      els.header.style.margin = "0 auto";
+      els.header.style.flexDirection = "row";
+      els.header.style.alignItems = "center";
+      els.headerLeftAImg.style.width = "60px";
+      els.headerRightA2.style.marginLeft = "20px";
+      els.headerRightA3.style.marginLeft = "20px";
+      els.headerRightA4.style.marginLeft = "20px";
+
       els.app.appendChild(els.header);
+      els.header.appendChild(els.headerLeft);
+      els.header.appendChild(els.headerRight);
+
+      els.headerLeft.appendChild(els.headerLeftA);
+      els.headerLeftA.appendChild(els.headerLeftAImg);
+      els.headerLeftAImg.src =
+        "https://fontmeme.com/permalink/210330/86265904da868351312d6fcc83be3480.png";
+      els.headerRight.appendChild(els.headerRightA1);
+      els.headerRightA1.innerHTML = "Home";
+      els.headerRight.appendChild(els.headerRightA2);
+      els.headerRightA2.innerHTML = "Skills";
+      els.headerRight.appendChild(els.headerRightA3);
+      els.headerRightA3.innerHTML = "Works";
+      els.headerRight.appendChild(els.headerRightA4);
+      els.headerRightA4.innerHTML = "Playground";
 
       //   els.main.appendChild(els.a1);
       //   lottie.loadAnimation({
@@ -30,21 +57,28 @@ const App = {
       //     animationData: A1,
       //   });
 
-      //   els.main.innerHTML = "main";
+      els.main.innerHTML = "main";
       els.app.appendChild(els.main);
 
-      els.footer.innerHTML = "footer";
-      els.app.appendChild(els.footer);
-
       console.log("Done");
+      console.log(app);
     },
   },
 
   elements: {
     app: document.getElementById("app"),
+
     header: document.createElement("div"),
+    headerLeft: document.createElement("div"),
+    headerRight: document.createElement("div"),
+    headerLeftA: document.createElement("a"),
+    headerLeftAImg: document.createElement("img"),
+    headerRightA1: document.createElement("a"),
+    headerRightA2: document.createElement("a"),
+    headerRightA3: document.createElement("a"),
+    headerRightA4: document.createElement("a"),
+
     main: document.createElement("div"),
-    footer: document.createElement("div"),
     a1: document.createElement("div"),
   },
 };
